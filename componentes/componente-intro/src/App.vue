fb395b4d961921f0ada51cd20492274871589037
andreflik <andreflik@gmail.com>
2021-08-08 09:45:27 -0300

    Curso vue components

--- componentes/componente-intro/src/App.vue
+++ componentes/componente-intro/src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
     <h1>Contadores</h1>
-    {{contador}}
+    <app-contadores></app-contadores>
+    <button>?</button>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      contador: 0
-    }
-  }
+  
 }
 </script>
 
