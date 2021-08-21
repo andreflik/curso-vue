de9a9d9936a86c3718c55c1f93b0737f1f6c2c50
andreflik <andreflik@gmail.com>
2021-08-21 10:43:22 -0300

    Citações - Slots e Componente Dinamico

---
+++ citacoes/src/components/Citacao.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot name="texto"></slot>
+        <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+   
+    // created(){
+    //     console.log('created')
+    // },
+    // destroyed() {
+    //     console.log('destroyed')
+    // },
+    // activated(){
+    //     console.log('activated')
+    // },
+    // deactivated() {
+    //     console.log('deactivated')
+    // }
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    /* ! important = ele prioriza o estilo que deve ser utilizado. */
+    h1 {
+        color: red  !important; 
+    }
+    h6 {
+        color:sienna;
+    }
+</style>
